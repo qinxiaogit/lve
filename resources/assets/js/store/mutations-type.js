76ee4a8f600257816b85dba406e1ba37eb6e75cf
Hui Lee <lihui870920@gmail.com>
2017-01-02 20:14:39 +0800

    Vuex项目结构

---
+++ resources/assets/js/store/mutations-type.js
@@ -0,0 +1,4 @@
+export const ADD_SOURCE     = 'ADD_SOURCE';
+export const SOURCE_REQUEST = 'SOURCE_REQUEST';
+export const ADD_CLIENT     = 'ADD_CLIENT';
+export const CLIENT_REQUEST = 'CLIENT_REQUEST';
